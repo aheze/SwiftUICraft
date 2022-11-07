d3173bff3a17905e26e2c8e674ed213429fffe5e
Zheng <aheze@getfind.app>
2022-11-06 19:19:03 -0800

    Initial Commit

---
+++ MC/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  MC
+//
+//  Created by A. Zheng (github.com/aheze) on 11/6/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC/MCApp.swift
@@ -0,0 +1,19 @@
+//
+//  MCApp.swift
+//  MC
+//
+//  Created by A. Zheng (github.com/aheze) on 11/6/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct MCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
